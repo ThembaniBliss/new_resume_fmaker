39610c69b84a5439120a2e04a3344e844749b9e5
Eulenda <ThembaniNG1_12@geeks4learning.com>
2024-07-18 01:38:24 +0200

    It is better but needs some polishing

--- lib/create_cv_screen.dart
+++ lib/create_cv_screen.dart
@@ -1,14 +1,11 @@
-// ignore_for_file: library_private_types_in_public_api, sort_child_properties_last, unused_field
+// ignore_for_file: sort_child_properties_last, use_super_parameters, library_private_types_in_public_api
 
 import 'package:flutter/material.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
 import 'package:printing/printing.dart';
 
-import 'ai_service.dart';
-
 class CreateCvScreen extends StatefulWidget {
-  // ignore: use_super_parameters
   const CreateCvScreen({Key? key}) : super(key: key);
 
   @override
@@ -18,124 +15,38 @@ class CreateCvScreen extends StatefulWidget {
 class _CreateCvScreenState extends State<CreateCvScreen> {
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _summaryController = TextEditingController();
+  final TextEditingController _profileController = TextEditingController();
   final TextEditingController _educationController = TextEditingController();
-  final TextEditingController _experienceController = TextEditingController();
-  final TextEditingController _skillsController = TextEditingController();
-  final TextEditingController _certificationsController =
+  final TextEditingController _dateOfBirthController = TextEditingController();
+  final TextEditingController _maritalStatusController =
       TextEditingController();
+  final TextEditingController _nationalityController = TextEditingController();
+  final TextEditingController _genderController = TextEditingController();
+  final TextEditingController _raceController = TextEditingController();
   final TextEditingController _languagesController = TextEditingController();
-
-  // Define suggestions lists
-  // ignore: prefer_final_fields
-  List<String> _experienceSuggestions = [
-    'Manager',
-    'Software Engineer',
-    'Project Manager'
-  ];
   final List<String> _skillsSuggestions = [
     'Programming',
     'Project Management',
     'Communication'
   ];
-  final List<String> _certificationsSuggestions = [
-    'PMP',
-    'AWS Certified',
-    'Scrum Master'
-  ];
-  final List<String> _languagesSuggestions = ['English', 'Spanish', 'French'];
-  List<String> _suggestions = [];
-
-  // Define the method to update suggestions based on input
-  void _updateSuggestions(String query, List<String> suggestionsSource) {
-    setState(() {
-      _suggestions = suggestionsSource
-          .where((suggestion) =>
-              suggestion.toLowerCase().contains(query.toLowerCase()))
-          .toList();
-    });
-  }
-
-  Future<void> _generateText(
-      TextEditingController controller, String prompt) async {
-    final generatedText = await AiService.generateText(prompt);
-    setState(() {
-      controller.text = generatedText;
-    });
-  }
 
   Widget buildTextField(
-    String label,
-    IconData icon,
-    TextEditingController controller, {
-    List<String>? suggestionsSource,
-    bool useAi = false,
-    String? aiPrompt,
-  }) {
+      String label, IconData icon, TextEditingController controller) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Expanded(
-                child: TextField(
-                  controller: controller,
-                  minLines: 1,
-                  maxLines: 5,
-                  decoration: InputDecoration(
-                    labelText: label,
-                    prefixIcon: Icon(icon),
-                    border: const OutlineInputBorder(),
-                    isDense: true,
-                  ),
-                  onChanged: suggestionsSource != null
-                      ? (query) => _updateSuggestions(query, suggestionsSource)
-                      : null,
-                ),
-              ),
-              if (useAi && aiPrompt != null)
-                IconButton(
-                  icon: const Icon(Icons.lightbulb),
-                  onPressed: () => _generateText(controller, aiPrompt),
-                ),
-            ],
+      padding: const EdgeInsets.only(bottom: 10),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          labelText: label,
+          prefixIcon: Icon(icon, color: Colors.deepPurple),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8.0),
+            borderSide: const BorderSide(color: Colors.deepPurple),
           ),
-          if (suggestionsSource != null && _suggestions.isNotEmpty)
-            Container(
-              constraints: const BoxConstraints(maxHeight: 200),
-              child: ListView.builder(
-                itemCount: _suggestions.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(_suggestions[index]),
-                    onTap: () {
-                      controller.text = _suggestions[index];
-                      setState(() {
-                        _suggestions = [];
-                      });
-                    },
-                  );
-                },
-              ),
-            ),
-        ],
-      ),
-    );
-  }
-
-  Widget buildButton(String text, VoidCallback onPressed) {
-    return Container(
-      width: double.infinity,
-      padding: const EdgeInsets.only(top: 10),
-      child: ElevatedButton(
-        onPressed: onPressed,
-        child: Text(text),
-        style: ElevatedButton.styleFrom(
-          foregroundColor: Colors.white,
-          backgroundColor: Colors.deepPurple,
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-          padding: const EdgeInsets.symmetric(vertical: 12),
+          focusedBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.deepPurple, width: 2),
+          ),
+          labelStyle: const TextStyle(color: Colors.deepPurple),
         ),
       ),
     );
@@ -143,19 +54,41 @@ class _CreateCvScreenState extends State<CreateCvScreen> {
 
   Future<void> generatePdf() async {
     final pdf = pw.Document();
-
     pdf.addPage(
       pw.Page(
+        theme: pw.ThemeData.withFont(
+          base: pw.Font.times(),
+          italic: pw.Font.timesItalic(),
+        ),
         build: (pw.Context context) {
           return pw.Column(
             crossAxisAlignment: pw.CrossAxisAlignment.start,
             children: [
-              pw.Text(
-                _nameController.text,
-                style:
-                    pw.TextStyle(fontSize: 24, fontWeight: pw.FontWeight.bold),
+              pw.Header(
+                level: 0,
+                child: pw.Text(_nameController.text,
+                    style: pw.TextStyle(
+                        fontSize: 24, fontWeight: pw.FontWeight.bold)),
               ),
-              // Other text components similar to this
+              pw.Text('Graphic Designer',
+                  style: const pw.TextStyle(fontSize: 18)),
+              pw.Divider(),
+              pw.Header(level: 1, child: pw.Text('Contact Information')),
+              pw.Text(_emailController.text),
+              pw.Text('123-456-7890'), // Placeholder
+              pw.Text('www.example.com'), // Placeholder
+              pw.Header(level: 1, child: pw.Text('Personal Details')),
+              pw.Bullet(text: 'Date of Birth: ${_dateOfBirthController.text}'),
+              pw.Bullet(
+                  text: 'Marital Status: ${_maritalStatusController.text}'),
+              pw.Bullet(text: 'Nationality: ${_nationalityController.text}'),
+              pw.Bullet(text: 'Gender: ${_genderController.text}'),
+              pw.Bullet(text: 'Race: ${_raceController.text}'),
+              pw.Bullet(text: 'Languages: ${_languagesController.text}'),
+              pw.Header(level: 1, child: pw.Text('Education')),
+              pw.Text(_educationController.text),
+              pw.Header(level: 1, child: pw.Text('Skills')),
+              ..._skillsSuggestions.map((skill) => pw.Bullet(text: skill)),
             ],
           );
         },
@@ -179,8 +112,23 @@ class _CreateCvScreenState extends State<CreateCvScreen> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             buildTextField('Name', Icons.person, _nameController),
-            // More fields similar to this
-            buildButton('Generate CV PDF', generatePdf),
+            buildTextField('Email', Icons.email, _emailController),
+            buildTextField('Profile', Icons.description, _profileController),
+            buildTextField('Date of Birth', Icons.cake, _dateOfBirthController),
+            buildTextField('Marital Status', Icons.family_restroom,
+                _maritalStatusController),
+            buildTextField('Nationality', Icons.flag, _nationalityController),
+            buildTextField('Gender', Icons.transgender, _genderController),
+            buildTextField('Race', Icons.run_circle, _raceController),
+            buildTextField('Languages', Icons.language, _languagesController),
+            buildTextField('Education', Icons.school, _educationController),
+            ElevatedButton(
+              onPressed: generatePdf,
+              child: const Text('Generate PDF'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.deepPurple, // Background color
+              ),
+            ),
           ],
         ),
       ),
